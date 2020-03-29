0fc8bf8f0443af3c0ece865dcf158fb9ad7f9555
Kevin Minutti <minutti70@gmail.com>
2020-03-29 18:56:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function viewCart() {
       if (index > 0 && index < cart.length) {
        var object = object
        //var middleitems = []
-       console.log(object["itemName"] + "at $"object["itemPrice"] + ",")
+       console.log(object["itemName"] + "at $" +  object["itemPrice"] + ","
       console.log(`${object["itemName"]} at $${object["itemPrice"]},`)
       };
       })
