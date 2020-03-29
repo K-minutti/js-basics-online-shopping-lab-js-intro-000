5d107f36a64a3ff081b8d1f7163e7ee10eda86d1
Kevin Minutti <minutti70@gmail.com>
2020-03-29 20:16:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
     var newArray = []
     for (let object = 0; object < cart.length; object++) {
       var index = [cart[index]]
-      if (index > 0 && index < cart.length) {
+      if (index = 0 && index < cart.length) {
         const middleObjects = `${object["itemName"]} at $${object["itemName"]},`;
         newArray.push(middleObjects);
     }
