5fe9ad424ec1c5e892d367d9e694df63102b9e56
Kevin Minutti <minutti70@gmail.com>
2020-03-29 17:23:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function viewCart() {
 for(let i =0, len=cart.length; i<len; i++ ) {
   //inner iterator
   for (let moreitems in cart[i]) {
-    return  `In your cart, you have ${cart[moreitems].itemName} at $${cart[moreitems].itemPrice},`
+    return  `In your cart, you have ${cart[i][moreitems]} at $${cart[i][moreitems]},`
 
 
   //  and ${cart[i].itemName} at $${cart[i].itemPrice}.`
