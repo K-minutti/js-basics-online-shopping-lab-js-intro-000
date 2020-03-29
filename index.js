62ce77236349cef0718f4005885d4ba1d3af844b
Kevin Minutti <minutti70@gmail.com>
2020-03-29 21:15:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,5 +52,6 @@ function placeOrder(cardNumber) {
   }
   else { return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
-  return cart = [];
+  cart = [];
+  return
 }
