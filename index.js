0c32a7e746c0b9cf8a109822aaa81cf9d08cad4d
Kevin Minutti <minutti70@gmail.com>
2020-03-29 18:00:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
   if (cart.length === 2) {
     for (let i in cart){
       for (twoItems in cart[i]){
-        return twoItems[0,1]
+        return twoItems[0]
       }
     }
   }
