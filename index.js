d5ef536c4850e03d03337d2099f1e2037989b20f
Kevin Minutti <minutti70@gmail.com>
2020-03-29 22:05:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeFromCart(item) {
    if (cardNumber == null) {
      return `Sorry, we don't have a credit card on file for you.`;}
    if (cardNumber !== null){
-    console.alert(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     return cart = [];
    }
  }
