3cdbf72c3620a9c447c0939383efe14f56835e47
Kevin Minutti <minutti70@gmail.com>
2020-03-29 20:10:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ function viewCart() {
       var index = [cart[index]]
       if (index > 0 && index < cart.length) {
         const middleObjects = `${object["itemName"]} at $${object["itemName"]},`;
-      //  newArray.push(middleObjects);
-      console.log("In your cart, you have " +  cart[0]["itemName"] " at $" + cart[0]["itemPrice"] + middleObjects + "and " + cart[-1]["itemName"] + " at $" + cart[-1]["itemPrice"] + ".");
+        newArray.push(middleObjects);
     }
       }
+    return  `In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}, ${newArray} and ${cart[-1]["itemName"]} at $${cart[-1]["itemPrice"]}.`
   }
 
-//In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}, ${newArray} and ${cart[-1]["itemName"]} at $${cart[-1]["itemPrice"]}.
+
 
 function removeFromCart(item) {}
 
