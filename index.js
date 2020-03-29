9ebf789a7be9ad7ccfdd0441338f4e182d35e95d
Kevin Minutti <minutti70@gmail.com>
2020-03-29 21:21:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber == null) {
     return "Sorry, we don't have a credit card on file for you.";
-  } else { return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;}
-  cart = [];
-  return
+  } else { return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  cart = [];}
 }
