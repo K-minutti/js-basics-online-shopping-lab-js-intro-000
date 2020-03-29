8d3f398daab78fb33f5472b7e03449305750df34
Kevin Minutti <minutti70@gmail.com>
2020-03-29 21:51:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function removeFromCart(item) {
   for (var n = 0; n < cart.length; n++) {
 
   var nameOfItem = cart[n]['itemName'];
-  
+
   if (item == nameOfItem) {
       cart.splice(n,1);
       return cart;}
