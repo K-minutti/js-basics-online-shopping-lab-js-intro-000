7789817384708c5574c4aacfe6f351f925891641
Kevin Minutti <minutti70@gmail.com>
2020-03-29 21:25:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function placeOrder(cardNumber) {
   if (cardNumber == null) {
     return "Sorry, we don't have a credit card on file for you.";}
   else {  console.log("Your total cost is " + total() + "," + " which will be charged to the card " + cardNumber + ".");
-  return cart = [];
+  cart = [];
 }
 
 }
