d87e93ee149824d4155d5b976324d5618a57e359
Kevin Minutti <minutti70@gmail.com>
2020-03-29 18:40:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ function viewCart() {
     }
   if (cart.length === 2) {
    return `In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}, and ${cart[1]["itemName"]} at $${cart[1]["itemPrice"]}.`}
-   
+
   if (cart.length > 2) {
     cart.forEach(function(object, index) {
       if (index > 0 && index < cart.length) {
-        console.log(object.itemName, object.itemPrice);  });
+        console.log(object.itemName, object.itemPrice);  };
       }
       //take the object.itemName & object.itemPrice of each object and create mutliple `itemName at $itemPrice,` statements and we want to push them all into a variable/array }
 
