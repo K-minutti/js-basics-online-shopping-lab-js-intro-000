235a202401484861772b81d27c1907d545e69369
Kevin Minutti <minutti70@gmail.com>
2020-03-29 21:15:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,11 +48,9 @@ function removeFromCart(item) {
 //function placeOrder(cardNumber) {}
 function placeOrder(cardNumber) {
   if (cardNumber === null) {
-    return `Sorry, we don't have a credit card on file for you.` ;
+    return `Sorry, we don't have a credit card on file for you.`;
   }
-  else {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  else { return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
   cart = [];
-  return
 }
